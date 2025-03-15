de78a958260b58b5848d45b9a51e7ecbc1b9b700
kavinda-100 <kavindarathnayake100@gmail.com>
2025-03-15 12:31:04 +0530

    Add array implementation in MainClass and update Program.cs to execute it

---
+++ DSA-cs-2025/Arrays/MainClass.cs
@@ -0,0 +1,21 @@
+﻿namespace DSA_cs_2025.Arrays;
+
+
+// DSA-cs-2025 for the Arrays Practice
+public class MainClass
+{
+    public void Run()
+    {
+        // create an array
+        int[] arr = new int[3];
+        // assign values to the array
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        // print the array
+        foreach (var t in arr)
+        {
+            System.Console.WriteLine(t);
+        }
+    }
+}
--- DSA-cs-2025/Program.cs
+++ DSA-cs-2025/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DSA_cs_2025.Arrays;
 
-Console.WriteLine("Hello, World!");
+//************************************************************************
+//? Array implementation for DSA-cs-2025
+MainClass main = new MainClass();
+main.Run();
+
+//************************************************************************
